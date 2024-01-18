50e5e56f597a8af7dc59e10df0ed00b644561b2e
Clenildon Ferreira <clenildonferreira34@gmail.com>
2024-01-18 16:06:30 -0300

    create project

---
+++ src/main/java/io/github/clenildonferreira/devtechnicaltestapi/DevTechnicalTestApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.clenildonferreira.devtechnicaltestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTechnicalTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTechnicalTestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/clenildonferreira/devtechnicaltestapi/DevTechnicalTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.clenildonferreira.devtechnicaltestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTechnicalTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
